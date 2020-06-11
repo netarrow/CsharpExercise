b0be2701eee0bd33bb01d01b110fb1544d4fdbee
MASSIDDA <MASSIDDA@MASSIDDA>
2020-06-11 11:01:00 +0200

    Added Automapping and UnitTest

--- MVC/UnitTestProject/UnitTest1.cs
+++ MVC/UnitTestProject/UnitTest1.cs
@@ -2,6 +2,11 @@
 using System.Linq;
 using GradeBook.Repository.DataBase;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Student = GradeBook.Repository.DataBase.Student;
+using Grade = GradeBook.Repository.DataBase.Grade;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using AutoMapper;
 
 namespace UnitTestProject
 {
@@ -21,6 +26,19 @@ namespace UnitTestProject
                 allStudent = ctx.Students;
                 Assert.AreEqual(1, allStudent.Count());
             }
+        }
+        [TestMethod]
+        public void LearningTest_Automapper()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Student, GradeBook.Models.Student>());
+            var mapper = config.CreateMapper();
+            Student student = new Student();
+            student.Id = 1;
+            student.Name = "William";
+            student.Grades = new List<Grade>() { new Grade() { Rate = 70, Subject = "Math", StudentId = 1 } };
+
+            GradeBook.Models.Student modelEntity = mapper.Map<GradeBook.Models.Student>(student);
+            Assert.AreEqual("William", modelEntity.Name);
         }
     }
 }
